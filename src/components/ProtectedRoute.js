2e1cb329954e4f737758f43080a69e48ed1b5153
DevWithEasy <devwitheasy@gmail.com>
2022-09-23 20:39:36 +0600

    local storage setup 2

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,10 +1,15 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
 import { Navigate, Outlet} from 'react-router';
+import { useDispatch } from 'react-redux';
+import { loginAction } from '../redux/actions/authAction';
 
 const ProtectedRoute = () => {
     // const user = useSelector(state=>state.auth.user)
+    const dispatch = useDispatch()
     const user = JSON.parse(localStorage.getItem('user'));
+    if(user){
+        dispatch(loginAction(user))
+    }
     return (
         <div>
             {
